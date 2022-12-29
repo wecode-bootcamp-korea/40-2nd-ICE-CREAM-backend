d400c3fc2aeb6e5f2c5b93f2d237d7148ba5b00d
minsik <minsik@minsiks-MacBook-Pro.local>
2022-12-29 16:37:54 +0900

    added login success unit test

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,4 +1,5 @@
 const request = require("supertest");
+const axios = require('axios');
 const { createApp } = require("../app");
 const AppDataSource = require("../src/models/data-source");
 
@@ -8,6 +9,13 @@ describe("Kakao Login", () => {
     beforeAll(async () => {
         app = createApp();
         await AppDataSource.initialize();
+    });
+
+    afterAll(async () => {
+        await AppDataSource.query(`SET FOREIGN_KEY_CHECKS = 0`);
+        await AppDataSource.query(`TRUNCATE users`);
+        await AppDataSource.query(`SET FOREIGN_KEY_CHECKS = 1`);
+        await AppDataSource.destroy();
     });
 
     test("FAILED: No Auth Code", async () => {
@@ -23,5 +31,31 @@ describe("Kakao Login", () => {
             .expect(400)
             .expect({ error: true, message: 'LOGIN_FAILED_KAKAO' });
     });
-    
+        
+    test('SUCCESS: Kakao Login', async () => {
+        axios.get = jest.fn().mockReturnValue({
+            data: 'bypass first axios.get'
+        })
+
+        axios.get = jest.fn().mockReturnValue({
+            data: {
+                id: 1010101010,
+                properties: {
+                  nickname: "닉네임",
+                  profile_image: "이미지.jpg"
+                },
+                kakao_account: {
+                  profile: {
+                    nickname: "닉네임",
+                  },
+                  email: "email@test.com",
+                },
+              }
+        })
+
+        await request(app)
+            .post("/users/login?code=testAuthCode")
+            
+        .expect(200);
+    })
 });
